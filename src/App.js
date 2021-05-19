2605a1af25036de041af2dd3180676ddd8a8d631
qkrtjdrbs <notlce@naver.com>
2021-05-20 00:47:47 +0900

    Sign up UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles, lightTheme } from "./screens/styles";
 import SignUp from "./screens/SignUp";
+import routes from "./routes";
 /*Switch select only one matched Route*/
 
 function App() {
@@ -17,11 +18,11 @@ function App() {
       <GlobalStyles />
       <Router>
         <Switch>
-          <Route path="/" exact>
+          <Route path={routes.home} exact>
             {isLogin ? <Home /> : <Login />}
           </Route>
           {!isLogin ? (
-            <Route path="/sign-up">
+            <Route path={routes.signUp}>
               <SignUp />
             </Route>
           ) : null}
---
+++ src/components/auth/Input.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  width: 100%;
+  border-radius: 3px;
+  padding: 7px;
+  background-color: #fafafa;
+  border: 0.5px solid rgb(219, 219, 219);
+  margin-top: 5px;
+  box-sizing: border-box;
+  &::placeholder {
+    font-size: 12px;
+  }
+  &:focus::placeholder {
+    color: transparent;
+  }
+`;
+
+export default Input;
